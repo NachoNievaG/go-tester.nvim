0b4560c07f0a611770a9ec7f7809157bcf23d3b5
Nacho Nieva <83428506+NachoNievaG@users.noreply.github.com>
2022-10-22 16:46:59 -0300

    feat(setup): add setup func, exec the cmd on first call (#4)
    * feat(setup): add setup func, exec the cmd on first call
    
    * fix(setup): major fixes
    
    * fix(print): removed prints
    
    * feat(doc): create configurations section
    
    * fix(doc): blankline oopsie

--- plugin/go-tester.lua
+++ plugin/go-tester.lua
@@ -1,14 +1,6 @@
 if vim.g.loaded_go_tester then
   return
 end
+
+require('go-tester').setup()
 vim.g.loaded_go_tester = true
-
-local command = vim.api.nvim_create_user_command
-
-command("GoTestPackageOnSave", function()
-  require("go-tester.suite").startPackageTest()
-end, {})
-
-command("GoTestFileOnSave", function()
-  require("go-tester.suite").startFileTest()
-end, {})
